f36fd141ef6bf623c99586382751892baee30c56
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-24 14:47:40 +0600

    provider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import BoardProvider from "./Contexts/Board.jsx";
+import TaskListsProvider from "./Contexts/TaskList.jsx";
+import TaskProvider from "./Contexts/Task.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BoardProvider>
+      <TaskListsProvider>
+        <TaskProvider>
+          <App />
+        </TaskProvider>
+      </TaskListsProvider>
+    </BoardProvider>
+  </React.StrictMode>
+);
