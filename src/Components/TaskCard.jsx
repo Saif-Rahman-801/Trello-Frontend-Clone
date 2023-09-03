2ae6f4205ebffe720e4345042a1319aae35469ef
Saif-Rahman-801 <saifurs102@gmail.com>
2023-09-03 12:14:23 +0600

    task list coding

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -11,7 +11,6 @@ const TaskCard = ({ task }) => {
   const { dispatchTaskListsAction } = useContext(TaskListsContext);
   const { dispatchBoardAction } = useContext(BoardContext);
 
-
   const submitHandler = (e) => {
     e.preventDefault();
     dispatchTaskAction({ type: "UPDATE_TASK", id: task.id, title: taskTitle });
@@ -20,32 +19,35 @@ const TaskCard = ({ task }) => {
 
   const removeTaskHandler = () => {
     dispatchTaskAction({ type: "DELETE_TASK", payload: { id: task.id } });
-    dispatchTaskListsAction({type: 'REMOVE_TASK_ID_FROM_LIST', payload: {id: task.listId, taskId: task.id}})
-    dispatchBoardAction({type: 'REMOVE_TASK_ID_FROM_A_BOARD', payload: {id: task.boardId, taskId: task.id}})
+    dispatchTaskListsAction({
+      type: "REMOVE_TASK_ID_FROM_LIST",
+      payload: { id: task.listId, taskId: task.id },
+    });
+    dispatchBoardAction({
+      type: "REMOVE_TASK_ID_FROM_A_BOARD",
+      payload: { id: task.boardId, taskId: task.id },
+    });
   };
 
-
-  return <div>
-    {
-        editMode ? (
-            <AddItemForm 
-            onChangeHandler={(e) => setTaskTitle(e.target.value)}
-            title={taskTitle}
-            setEditMode={setEditMode}
-            submitHandler={submitHandler}
-            /> 
-        ):(
-            <div onClick={(e) => setEditMode(true)} className="task-card">
-                <p>
-                    {taskTitle}
-                </p>
-                <button onClick={removeTaskHandler} className="add-item-icon">
-                    Del
-                </button>
-            </div>
-        )
-    }
-  </div>;
+  return (
+    <div>
+      {editMode ? (
+        <AddItemForm
+          onChangeHandler={(e) => setTaskTitle(e.target.value)}
+          title={taskTitle}
+          setEditMode={setEditMode}
+          submitHandler={submitHandler}
+        />
+      ) : (
+        <div onClick={(e) => setEditMode(true)} className="task-card">
+          <p>{taskTitle}</p>
+          <button onClick={removeTaskHandler} className="add-item-icon">
+            Del
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TaskCard;
--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { TaskContext } from "../Contexts/Task";
+import { TaskListsContext } from "../Contexts/TaskList";
+import { BoardContext } from "../Contexts/Board";
+import TaskCard from "./TaskCard";
+import AddItemForm from "./AddItemForm";
+import AddItem from "./AddItem";
 
-const TaskList = () => {
-    return (
-        <div>
-            
+const TaskList = ({ taskList }) => {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [editMode, setEditMode] = useState(false);
+
+  const { title } = taskList;
+  const { tasks: allTasks, dispatchTaskAction } = useContext(TaskContext);
+  const { dispatchTaskListsAction } = useContext(TaskListsContext);
+  const { dispatchBoardAction } = useContext(BoardContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const id = shortId.generate();
+    const listId = taskList.id;
+    const task = {
+      id,
+      title: taskTitle,
+      listId,
+      boardId: taskList.boardId,
+    };
+    dispatchTaskAction({ type: "CREATE_TASK", payload: task });
+    dispatchTaskListsAction({
+      type: "ADD_TASK_ID_TO_LIST",
+      payload: { id: listId, taskId: task.id },
+    });
+    dispatchBoardAction({
+      type: "ADD_TASK_ID_TO_BOARD",
+      payload: { id: task.boardId, taskId: task.id },
+    });
+    setTaskTitle("");
+    setEditMode(false);
+  };
+
+  const removeListHandler = () => {
+    dispatchTaskListsAction({
+      type: "DELETE_LIST",
+      payload: { id: taskList.id },
+    });
+    dispatchBoardAction({
+      type: "REMOVE_LIST_ID_FROM_A_BOARD",
+      payload: { id: taskList.boardId, listId: taskList.id },
+    });
+  };
+
+  return (
+    <div>
+      <div className="list-container">
+        <div className="list-titleContainer">
+          <h5>{title} </h5>
+          <button onClick={removeListHandler} className="add-item-icon">
+            Del
+          </button>
         </div>
-    );
+        {taskList?.tasks
+          ?.map((item) => {
+            return allTasks.find((t) => t.id === item);
+          })
+          ?.map((task, index) => (
+            <TaskCard
+              index={index}
+              id={task.id}
+              taskList={taskList}
+              task={task}
+            />
+          ))}
+        {editMode ? (
+          <AddItemForm
+            submitHandler={submitHandler}
+            title={taskTitle}
+            onChangeHandler={(e) => setTaskTitle(e.target.value)}
+            setEditMode={setEditMode}
+            editMode={editMode}
+          />
+        ) : (
+          <AddItem setEditMode={setEditMode} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default TaskList;
